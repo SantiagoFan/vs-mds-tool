af2911bd808253bd84ab885bbbc9331fb6662d97
Administrator <san_fan@qq.com>
2021-11-04 00:14:18 +0800

    x

--- extension.js
+++ extension.js
@@ -28,6 +28,19 @@ function activate(context) {
 		vscode.window.showInformationMessage('大家好!');
 		console.info(uri)
 	}));
+	// 欢迎页
+	context.subscriptions.push(vscode.commands.registerCommand('mds-tool.showWelcome',function(){
+		const panel = vscode.window.createWebviewPanel(
+			'testWebview', // viewType
+			"WebView演示", // 视图标题
+			vscode.ViewColumn.One, // 显示在编辑器的哪个部位
+			{
+				enableScripts: true, // 启用JS，默认禁用
+				retainContextWhenHidden: true, // webview被隐藏时保持状态，避免被重置
+			}
+		);
+		panel.webview.html = `<html><body>你好，我是Webview</body></html>`
+	}))
 	
 }
 
